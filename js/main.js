5cd3e8675fc0c328009f49980aab9281b4881291
Osvaldo Gago <opgago@yahoo.com>
2020-05-02 16:37:38 +0200

    Basic logic

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/* jshint browser: true,  esversion: 6 */
+/* global jQuery */
+
+/**
+ * Get dimension value
+ * @param   {string} dimension Dimension name
+ * @returns {number} Dimension value
+ */
+function getDimensionValue(dimension) {
+    if (sessionStorage.getItem(dimension) === null) {
+        return 0;
+    } else {
+        return Number(sessionStorage.getItem(dimension));    
+    }
+    
+}
+
+/**
+ * Delete dimension
+ * @param {string} dimension Dimension name
+ */
+function deleteDimension(dimension){
+    sessionStorage.removeItem(dimension);
+}
+
+/**
+ * Add value to dimension
+ * @param {string} dimension Dimension name
+ * @param {number} value     Value to add / subtract
+ */
+function addToDimension(dimension, value) {
+    let dimensionValue = getDimensionValue(dimension);
+    dimensionValue = dimensionValue + value;
+    sessionStorage.setItem(dimension, dimensionValue);
+}
+
+jQuery(".answer button").on("click", function () {
+    const dimension1 = jQuery(this).data("dimension1");
+    const dimension2 = jQuery(this).data("dimension2");
+    addToDimension("dimension1", dimension1);
+    addToDimension("dimension2", dimension2);
+});
